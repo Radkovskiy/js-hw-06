15a2e4b6ac9c276a0dacb8d9eca85f00a4e74d6a
Radkovskiy <radkovskiy97@gmail.com>
2023-01-30 22:04:12 +0200

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientslesList = document.querySelector("#ingredients")
+console.log(ingredientslesList);
+
+ingredients.map(el => {
+  const ingredientEl = document.createElement("li")
+  ingredientEl.textContent = el
+  ingredientslesList.append(ingredientEl)
+})
