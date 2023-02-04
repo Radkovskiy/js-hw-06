266b003715c188bc0c89d97749e9c3f305cc48e2
Radkovskiy <radkovskiy97@gmail.com>
2023-02-04 23:31:21 +0200

    task-06

---
+++ src/js/task-06.js
@@ -0,0 +1,15 @@
+const input = document.querySelector('#validation-input');
+
+input.addEventListener('blur', validation);
+
+function validation() {
+    if (input.value.length === Number(input.dataset.length)) {
+        input.classList.add('valid')
+        input.classList.remove('invalid')
+    }
+
+    else {
+        input.classList.add('invalid')
+        input.classList.remove('valid')
+    }
+}
