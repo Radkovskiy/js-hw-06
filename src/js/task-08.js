6dfbe536dbbfb7e1177319f5729fdf35c2a2c3da
Radkovskiy <radkovskiy97@gmail.com>
2023-02-05 13:57:25 +0200

    task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const objectValues = {};
+    const formElements = event.currentTarget.elements;
+
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = {
+        email: mail,
+        password: password
+    }
+
+    if (mail === "" || password === "") {
+        alert("все поля должны быть заполнены")
+    }
+    else {
+        console.log(formData);
+        form.reset()
+    }
+}
