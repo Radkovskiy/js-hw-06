d7f7e49fcebabd9d2fc5f147fc9e718a24fea48c
Radkovskiy <radkovskiy97@gmail.com>
2023-02-06 22:55:59 +0200

    task-10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,7 +3,31 @@ function getRandomHexColor() {
 }
 
 const input = document.querySelector('.input')
+const createBtn = document.querySelector('.createBtn')
+const destroyBtn = document.querySelector('.destroyBtn')
+const boxesContainer = document.querySelector('#boxes')
 
-input.addEventListener('input', (event) => {
-  input.textContent = event.currentTarget.value;
+
+createBtn.addEventListener('click', () => {
+
+  const inputValue = input.value;
+  const colorsArr = [];
+  let scale = 20
+
+  for (let i = 0; i < inputValue; i += 1) {
+    colorsArr.push(getRandomHexColor());
+  }
+
+  colorsArr.map((el) => {
+    const colorBlock = document.createElement("div");
+    scale += 10
+    colorBlock.style.width = `${scale}px`;
+    colorBlock.style.height = `${scale}px`;
+    colorBlock.style.backgroundColor = el;
+    boxesContainer.append(colorBlock);
+  })
+
+  destroyBtn.addEventListener('click', () => {
+    boxesContainer.innerHTML = ""
+  })
 })
