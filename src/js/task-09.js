509ec3aff646555d45cf2a5fc5b7b7b02e1998fb
Radkovskiy <radkovskiy97@gmail.com>
2023-02-06 12:17:15 +0200

    task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const buttonChangeColor = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+
+// почему заработало тогда, когда я добавил стрелку, 
+// но когда я писал название вункции, то онон не работало?
+// опять тупил со скобками при вызове функции....
+buttonChangeColor.addEventListener('click', () => {
+  const generateColor = getRandomHexColor()
+  bodyEl.style.backgroundColor = generateColor
+  colorName.textContent = generateColor
+})
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,9 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('.input')
+
+input.addEventListener('input', (event) => {
+  input.textContent = event.currentTarget.value;
+})
